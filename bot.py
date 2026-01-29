4e6ecc6d3fca70848aff2bfba6548592354508ea
stellarjaina <stellarjaina@gmail.com>
2026-01-29 15:38:50 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from collector_cog import Collector
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} ({bot.user.id})")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands.")
+    except Exception as e:
+        print("Command sync error:", e)
+
+
+@bot.event
+async def setup_hook():
+    await bot.add_cog(Collector(bot))
+
+
+bot.run("token")
